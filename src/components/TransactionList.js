8f319b619daf6d90ddd6da3bcb197e63c8a05af6
Anteneh Tilaye <antenehtilayekebede@gmail.com>
2024-01-17 04:55:06 -0800

    expense tracer

---
+++ src/components/TransactionList.js
@@ -0,0 +1,14 @@
+import React,{useContext} from 'react'
+import { GlobalContext } from './context/GlobalState'
+export const TransactionList = () => {
+  return (
+    <>
+     <h3>History</h3>
+     <ul className="list">
+       
+        
+     </ul>     
+    </>
+  )
+}
+
